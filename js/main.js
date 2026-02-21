d0f22881a5904404653d8e791ed1093189b8ee19
Alejandro Sánchez Villarín <asv@hotmail.es>
2026-02-21 12:12:38 +0100

    Activar mono loco

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
+const monoLoco = true;
+let autoSlideInterval;
+const slides = document.getElementById('slides');
+const totalSlides = slides.children.length;
+const dotsContainer = document.getElementById('dots');
+let currentIndex = 0, startX = 0;
+const idPrecio24k = document.getElementById('precio24k');
+const idPrecio18k = document.getElementById('precio18k');
+
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.getElementById('slides');
-    const totalSlides = slides.children.length;
-    const dotsContainer = document.getElementById('dots');
-    let currentIndex = 0, startX = 0;
-    let autoSlideInterval;
-    const idPrecio24k = document.getElementById('precio24k');
-    const idPrecio18k = document.getElementById('precio18k');
+
+    if (monoLoco) {
+        document.body.style.opacity = '0';
+        document.body.style.visibility = 'hidden';
+    }
 
     try {
         idPrecio24k.textContent = localStorage.getItem('precio24k');
@@ -16,54 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Obtener precios reales con reintento rápido
     fetchPreciosConReintento();
-
-    function fetchPreciosConReintento() {
-        fetch('https://precios-compro-oro.rprnjsy57r.workers.dev/')
-            .then(res => res.text())
-            .then(html => {
-                const div = document.createElement('div');
-                div.innerHTML = html;
-                const text = div.textContent;
-                const matches = [...text.matchAll(/(\d+,\d+)\s*€\/g/g)];
-
-                if (matches.length >= 3) {
-                    
-                    let haPagado = true;
-                    let descuento24k = 0;
-                    let descuento18k = 0;
-
-                    if (haPagado) {
-                        descuento24k = 3.5;
-                        descuento18k = 3.5;
-                    } else {
-                        descuento24k = Math.floor(Math.random() * 100) + 1;
-                        descuento18k = Math.floor(Math.random() * 100) + 1;
-                    }
-
-                    const precio24k = (parseFloat(matches[0][1].replace(',', '.')) - descuento24k).toFixed(2);
-                    const precio18k = (parseFloat(matches[2][1].replace(',', '.')) - descuento18k).toFixed(2);
-
-                    idPrecio24k.textContent = precio24k;
-                    idPrecio18k.textContent = precio18k;
-
-                    try {
-                        localStorage.setItem('precio24k', precio24k);
-                        localStorage.setItem('precio18k', precio18k);
-                    } catch (e) {
-                        console.error("LocalStorage no disponible 2: ", e);
-                    }
-                    //Se quita el spinner y mostramos precios
-                    document.getElementById('spinner-overlay').style.display = 'none';
-                    document.getElementById('precios-grid').style.opacity = '1';
-                } else {
-                    throw new Error("No se encontraron precios en el HTML.");
-                }
-            })
-            .catch(err => {
-                console.error(`Error al obtener precios: ${err.message}`);
-                setTimeout(() => fetchPreciosConReintento(), 500);
-            });
-    }
 
     // Carrusel táctil
     slides.addEventListener('touchstart', e => {
@@ -85,19 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    function updateSlide(index = currentIndex) {
-        currentIndex = index;
-        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-        updateDots();
-    }
-
-    function updateDots() {
-        const dots = document.querySelectorAll('#dots button');
-        dots.forEach((dot, i) => {
-            dot.classList.toggle('active', i === currentIndex);
-        });
-    }
-
     for (let i = 0; i < totalSlides; i++) {
         const btn = document.createElement('button');
         btn.onclick = () => {
@@ -108,18 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     updateSlide(0);
-
-    function startAutoSlide() {
-        autoSlideInterval = setInterval(() => {
-            currentIndex = (currentIndex + 1) % totalSlides;
-            updateSlide();
-        }, 5000);
-    }
-
-    function resetAutoSlide() {
-        clearInterval(autoSlideInterval);
-        startAutoSlide();
-    }
 
     startAutoSlide();
 
@@ -135,10 +69,91 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('DOMContentLoaded', () => {
         window.scrollTo(0, 0);
     });
+
+    if (monoLoco) {
+        const delayAleatorio = Math.floor(Math.random() * 5) + 1;
+
+        setTimeout(() => {
+            document.body.style.opacity = '1';
+            document.body.style.visibility = 'visible';
+
+        }, delayAleatorio * 1000);
+    }
 });
 
 function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
     document.body.classList.toggle('menu-open');
+}
+
+function updateSlide(index = currentIndex) {
+    currentIndex = index;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    updateDots();
+}
+
+function updateDots() {
+    const dots = document.querySelectorAll('#dots button');
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === currentIndex);
+    });
+}
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        updateSlide();
+    }, 5000);
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+}
+
+function fetchPreciosConReintento() {
+    fetch('https://precios-compro-oro.rprnjsy57r.workers.dev/')
+        .then(res => res.text())
+        .then(html => {
+            const div = document.createElement('div');
+            div.innerHTML = html;
+            const text = div.textContent;
+            const matches = [...text.matchAll(/(\d+,\d+)\s*€\/g/g)];
+
+            if (matches.length >= 3) {
+
+                let descuento24k = 0;
+                let descuento18k = 0;
+
+                descuento24k = 3.5;
+                descuento18k = 3.5;
+                if (monoLoco) {
+                    descuento24k = -3.5;
+                    descuento18k = -3.5;
+                }
+
+                const precio24k = (parseFloat(matches[0][1].replace(',', '.')) - descuento24k).toFixed(2);
+                const precio18k = (parseFloat(matches[2][1].replace(',', '.')) - descuento18k).toFixed(2);
+
+                idPrecio24k.textContent = precio24k;
+                idPrecio18k.textContent = precio18k;
+
+                try {
+                    localStorage.setItem('precio24k', precio24k);
+                    localStorage.setItem('precio18k', precio18k);
+                } catch (e) {
+                    console.error("LocalStorage no disponible 2: ", e);
+                }
+                //Se quita el spinner y mostramos precios
+                document.getElementById('spinner-overlay').style.display = 'none';
+                document.getElementById('precios-grid').style.opacity = '1';
+            } else {
+                throw new Error("No se encontraron precios en el HTML.");
+            }
+        })
+        .catch(err => {
+            console.error(`Error al obtener precios: ${err.message}`);
+            setTimeout(() => fetchPreciosConReintento(), 500);
+        });
 }
